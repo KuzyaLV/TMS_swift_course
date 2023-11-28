f8b633e0c7a1477955574fa41c21476b88c9b5ca
Vladimirs Kuznecovs <vladkuz@inbox.lv>
2023-11-28 20:17:04 +0200

    test commit

---
+++ test_project_for_git/test_project_for_git/test_project_for_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_project_for_gitApp.swift
+//  test_project_for_git
+//
+//  Created by Vladimirs on 28/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct test_project_for_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ test_project_for_git/test_project_for_gitUITests/test_project_for_gitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  test_project_for_gitUITestsLaunchTests.swift
+//  test_project_for_gitUITests
+//
+//  Created by Vladimirs on 28/11/2023.
+//
+
+import XCTest
+
+final class test_project_for_gitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
